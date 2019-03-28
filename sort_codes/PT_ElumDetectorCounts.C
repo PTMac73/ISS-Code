e0f1e72633d115c28827595f14135c65e8d36afa
Patrick MacGregor <patrick.macgregor@postgrad.manchester.ac.uk>
2019-03-28 11:04:28 +0000

    Added elum detector file

---
+++ sort_codes/PT_ElumDetectorCounts.C
@@ -0,0 +1,9 @@
+// PT_ElumDetectorCounts.C
+// Gets all stuff relevant to deuteron scattering etc.
+// ============================================================================================= //
+// Patrick MacGregor
+// Nuclear Physics Research Group
+// School of Physics and Astronomy
+// The University of Manchester
+// ============================================================================================= //
+
